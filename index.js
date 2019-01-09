8e5a88a7bc813e4684711ff4a0b2f3082f54a457
Marc Szewczyk <marc.szewczyk@gmail.com>
2019-01-08 21:30:59 -0500

    Changed hello message.

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function (request, response) {
-  response.send('Something else')
+  response.send('Hello from GUI pipeline!')
 })
 
 app.listen(app.get('port'), function () {
