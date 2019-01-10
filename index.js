568b563afd3d3a0d9c3db902cd11ba00f561d7f0
Marc Szewczyk <marc.szewczyk@gmail.com>
2019-01-09 21:34:35 -0500

    Updated version and added test. Updated pipeline script.

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function (request, response) {
-  response.send('Something else')
+  response.send('Hello from simple pipeline!')
 })
 
 app.listen(app.get('port'), function () {
